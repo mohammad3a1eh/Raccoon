30a59230d90ac1f59c959d71bdbfd59802071e50
Mohammad Saleh <kinitegp@gmail.com>
2024-11-28 11:20:30 +0330

    add memmoryleaks module

---
+++ module/module_memoryleaks_rss.py
@@ -0,0 +1,86 @@
+import json
+import os
+import module
+import module.tbot
+import feedparser
+import asyncio
+
+FILENAME = os.path.splitext(os.path.basename(__file__))[0]
+
+NAME = "MemoryLeaks"
+DESCRPTION = """
+Written on 1403/07/06
+"""
+
+
+def load_(data):
+    with open(f"./database/{data}.json", "r") as file:
+        DATA_S = json.loads(file.read())
+
+    return DATA_S
+
+
+def read_rss():
+    url = f"https://memoryleaks.ir/feed"
+    feed = feedparser.parse(url)
+    return feed
+
+# hamed_ebrahimi66
+
+
+def post_exists_in_db(link, db):
+    for item in db:
+        if link['link'] == item['link']:
+            return True
+    return False
+
+async def run(token, chat_id):
+    db = load_(data="memoryleaks")
+
+    module.tbot.Telegram.init(token=token)
+    await module.tbot.Telegram.send_text_to_tel(f"The raccoon script has started running the '{NAME}' module",chat_id=chat_id)
+
+    post_ = 0
+
+    rss_feed = read_rss()
+
+    for entry in rss_feed.entries:
+        if int(post_ / 5) == 0:
+            await asyncio.sleep(15)
+        else:
+            await asyncio.sleep(10)
+
+        post = {
+            "link": entry.link
+        }
+
+        if post_exists_in_db(link=post, db=db):
+            continue
+
+        post_ += 1
+
+        title = entry.title
+        author = entry.author if hasattr(entry, 'author') else "Unknown"
+
+        text = f"""
+<b>{title}</b>(<i>{author}</i>)
+<a href='{post['link']}'>Link</a>"""
+
+        links = [
+            [
+                {
+                    "text": "",
+                    "url": f"{post['link']}"
+                }
+            ],
+        ]
+        
+        module.tbot.Telegram.send_text_with_btn(text, links, chat_id, token)
+
+        db.append(post)
+
+        with open(f"database/memoryleaks.json", "w") as file:
+            json.dump(db, file, indent=4)
+
+        await module.tbot.send_text_to_tel(f"Find {post_} post", chat_id)
+
